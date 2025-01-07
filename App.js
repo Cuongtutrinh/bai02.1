793be69bb3fcb5a5e11390fcaf33afc6516604d6
Cuong Tu Trinh <cuongtt.22810310199@epu.edu.vn>
2025-01-07 10:13:10 +0700

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Ô vuông */}
+      <View style={styles.square}>
+        <Text style={styles.text}>Hello, world</Text>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, // Chiếm toàn bộ màn hình
+    justifyContent: 'center', // Căn giữa theo chiều dọc
+    alignItems: 'center', // Căn giữa theo chiều ngang
+    backgroundColor: 'white',// Đặt màu nền toàn bộ màn hình là trắng
+  },
+  square: {
+    width: 100, // Chiều rộng ô vuông
+    height: 100, // Chiều cao ô vuông
+    backgroundColor: 'skyblue', // Màu nền
+    justifyContent: 'center', // Căn giữa nội dung bên trong (văn bản)
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    color: 'white', // Màu chữ
+    fontSize: 16, // Kích thước chữ
   },
 });
+
+export default App;
